00a6936f995b948df4925d6eb21d946dd68f0ba7
DESKTOP-AM12U9O\MinYoung <qo613@naver.com>
2020-08-23 12:22:48 +0900

    cookie&session example

---
+++ src/com/practice/cookie/loginCon.java
@@ -0,0 +1,47 @@
+package com.practice.cookie;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/CookieEx/loginCon")
+public class loginCon extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		String mId = request.getParameter("mId");
+		String mPw = request.getParameter("mPw");
+		out.print("mId : " + mId);
+		out.print("mPw : " + mPw);
+		
+		
+		Cookie[] cookies = request.getCookies();
+		Cookie cookie = null;
+		for(Cookie c : cookies) {
+			if(c.getName().equals("memberId")) {
+				cookie = c;
+			}
+		}
+		
+		if(cookie == null) {
+			cookie = new Cookie("memberId", mId);
+		}
+		
+		response.addCookie(cookie);
+		cookie.setMaxAge(60 * 60);
+		
+		response.sendRedirect("loginOk.jsp");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/com/practice/session/loginCon.java
@@ -0,0 +1,35 @@
+package com.practice.session;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/SessionEx/loginCon")
+public class loginCon extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		String mId = request.getParameter("mId");
+		String mPw = request.getParameter("mPw");
+		out.print("mId : " + mId);
+		out.print("mPw : " + mPw);
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("memberId", mId);
+		
+		response.sendRedirect("loginOk.jsp");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
